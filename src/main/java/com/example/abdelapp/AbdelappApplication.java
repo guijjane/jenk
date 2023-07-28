16f4cf1fb6cdb1452508c477a00e583c84703579
guijjane <guijjaneabdelghaffar85@gmail.com>
2023-07-28 10:13:12 +0100

    jenkins exo eval

---
+++ src/main/java/com/example/abdelapp/AbdelappApplication.java
@@ -0,0 +1,13 @@
+package com.example.abdelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbdelappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/abdelapp/AbdelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abdelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
